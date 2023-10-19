b634f59804d38fab46ddeb5278214d85e41778cc
mdrabiulis <mdrabiulis155@gmail.com>
2023-10-20 01:49:27 +0600

    Brand Name Find products with mathot add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,10 +11,10 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS_KEY}@cluster0.ltbxnxt.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,10 +25,69 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    client.connect();
+    const AssignmentData = client.db("assignmentDB").collection("assignmentdata");
+
+
+
+
+  app.get("/products", async(req, res) =>{
+    const cursor = AssignmentData.find();
+    const result = await cursor.toArray();
+    res.send(result)
+  
+  })
+
+
+// product
+ 
+
+
+
+  app.get("/products/:BrandName", async(req, res) =>{
+    const Brandname = req.params.BrandName;
+    const query ={BrandName: (Brandname)};
+    const cursor = AssignmentData.find(query);
+    const result = await cursor.toArray();
+    res.send(result)
+    // console.log(result);
+  
+  })
+
+
+ 
+
+  app.post("/products", async (req, res) => {
+    const productsData = req.body;
+    const result = await AssignmentData.insertOne(productsData);
+    res.send(result);
+    // console.log(productsData);
+  });
+
+
+
+
+
+
+
+// put
+
+
+
+
+  app.delete('/products/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await AssignmentData.deleteOne(query);
+    res.send(result);
+  })
+
+
+
+
+
+
+    client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
